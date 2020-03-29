32ded9531f63af062282138b8d4617d16b75391d
LOGI <vcheckzen@outlook.com>
2020-03-29 22:50:19 +0800

    Update loliloli.user.js

--- loliloli.user.js
+++ loliloli.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                哔哩哔哩解析辅助
 // @namespace           https://github.com/vcheckzen/UnblockBilibili/blob/master/loliloli.user.js
-// @version             0.0.6.5
+// @version             0.0.6.5.1
 // @icon                https://www.bilibili.com/favicon.ico
 // @description         为哔哩哔哩视频注入一键解析按钮
 // @author              https://github.com/vcheckzen
@@ -44,49 +44,36 @@
         if (/.+ep\d+.+/.test(location.href)) {
             analysisServer += location.href.split('?')[0];
         } else if (/.+ss\d+.+/.test(location.href)) {
-            let id = __INITIAL_STATE__.epInfo.id
-            if (__PGC_USERSTATE__.hasOwnProperty('progress')) {
-                id = __PGC_USERSTATE__.progress.last_ep_id;
-            }
+            const id = __INITIAL_STATE__.epInfo.id;
             analysisServer += bilibiliHost + 'bangumi/play/ep' + id;
         } else if (/.+BV1\w+.+/.test(location.href)) {
             const p = new URLSearchParams(location.search).get('p');
-            analysisServer += bilibiliHost + 'video/' + __INITIAL_STATE__.videoData.bvid + (p ? '&p=' + p : '');
+            const bvid = __INITIAL_STATE__.videoData.bvid;
+            analysisServer += bilibiliHost + 'video/' + bvid + (p ? '&p=' + p : '');
         }
         window.open(analysisServer);
     };
 
-    const waitElement = function (selector, callback, fail, interval, timeout) {
+    const waitElements = function (selector, callback, fail, interval, timeout) {
         interval = interval || 500;
         timeout = timeout || 10000;
-        let elem = null, iWaitor = null, tWaitor = null;
+        let elems = null, iWaitor = null, tWaitor = null;
         iWaitor = setInterval(() => {
-            elem = document.querySelector(selector);
-            if (elem) {
+            elems = document.querySelectorAll(selector);
+            if (elems.length) {
                 clearInterval(iWaitor);
                 clearTimeout(tWaitor);
-                if (!payVideo()) {
-                    callback(elem);
-                }
+                if (!payVideo()) elems.forEach(elem => callback(elem));
             }
         }, interval);
         tWaitor = setTimeout(() => {
             clearInterval(iWaitor);
             clearTimeout(tWaitor);
-            if (typeof fail === 'function') {
-                fail();
-            }
+            if (typeof fail === 'function') fail();
         }, timeout);
     };
 
     const registerAnalysisButton = function registerAnalysisButton() {
-        const hintText = '解析';
-        waitElement('.twp-btn.right.vip', elem => {
-            const cloneNode = elem.cloneNode(true);
-            cloneNode.innerHTML = '一键' + hintText;
-            cloneNode.onclick = redirectToAnalysisServer;
-            elem.parentElement.replaceChild(cloneNode, elem);
-        });
         if (!document.head.querySelector('#style-loliloli')) {
             document.head.append(document.createRange().createContextualFragment(
                 `<style id="style-loliloli">
@@ -102,7 +89,16 @@
                 </style>`
             ).firstElementChild);
         }
-        waitElement('.bilibili-player-video-top-issue', elem => {
+
+        const hintText = '解析';
+        waitElements('.twp-btn.right.vip', elem => {
+            const cloneNode = elem.cloneNode(true);
+            cloneNode.innerHTML = '一键' + hintText;
+            cloneNode.onclick = redirectToAnalysisServer;
+            elem.parentElement.replaceChild(cloneNode, elem);
+        });
+
+        waitElements('.bilibili-player-video-top-issue', elem => {
             if (document.querySelector('.btn-anls')) return;
             elem.parentElement.insertBefore(document.createRange().createContextualFragment(
                 `<div class="btn-anls"><span style="margin: 0 2em;">${hintText}</span></div>`
@@ -110,11 +106,17 @@
             document.querySelector('.btn-anls').addEventListener('click', redirectToAnalysisServer);
         });
 
+        waitElements('button[class*="fullscreen"]', elem => {
+            elem.addEventListener('click', () => {
+                const btn = document.querySelector('.btn-anls');
+                if (btn && !player.isFullScreen()) btn.style.display = 'none';
+                else btn.style.display = 'initial';
+            });
+        });
+
         rightLists.forEach(selector => {
             const el = document.querySelector(selector);
-            if (el) {
-                el.addEventListener('click', registerAnalysisButton);
-            }
+            if (el) el.addEventListener('click', registerAnalysisButton);
         });
     };
 
